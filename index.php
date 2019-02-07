7b261ab39f94546b078d4ff61c1bbe9db52eef95
Lamar <lamarharrisdesigns@gmail.com>
2019-02-06 20:58:08 -0600

    Current Live Site

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lamar Harris Designs</title>
+	<meta name="description" content="Lamar Harris' Online Portfolio" />
+	<meta name="keywords" content="HTML, CSS, HTML5, CSS3, JavaScript, Designer, jQuery, Logos, Visual, Illustrator, Wireframing, Mobile Responsive, Front–end, Web Development, Interface, Concept, Project Manager, Marketing, Creative Graphics " />
+	<meta name="author" content="Lamar Harris" />
+	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+    <link rel="stylesheet" href="css/animations.css">
+    
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script src="js/modernizr-custom.js"></script>
+</head>
+
+<body>
+	<!-- navigation -->
+	<nav class="pages-nav">
+		<div class="pages-nav__item"><a class="link link--page" href="#page-home">Main</a></div>
+		<div class="pages-nav__item"><a class="link link--page" href="#page-lamar">Lamar</a></div>
+        <div class="pages-nav__item"><a class="link link--page" href="#page-work">Work</a></div>
+        <div class="pages-nav__item"><a class="link link--page" href="#page-contact">Contact</a></div>
+	
+		
+		<div class="pages-nav__item pages-nav__item--social">
+			<a class="link link--social link--faded" href="assets/Harris Resume 2017.pdf" target="_blank"><i class="fa fa-file-pdf-o"></i><span class="text-hidden">Resumé</span></a>
+            <a class="link link--social link--faded" href="mailto:lamarharrisdesigns@gmail.com"><i class="fa fa-envelope"></i><span class="text-hidden">Email Address</span></a>
+            <a class="link link--social link--faded" href="https://www.linkedin.com/in/lamar-harris-4638b4135/"  target="_blank"><i class="fa fa-linkedin"></i><span class="text-hidden">LinkedIn</span></a>
+			<a class="link link--social link--faded" href="https://github.com/lamarharrisdesigns" target="_blank"><i class="fa fa-github-alt"></i><span class="text-hidden">GitHub</span></a>
+		</div>
+	</nav>
+	<!-- /navigation-->
+	<!-- pages stack -->
+	<div class="pages-stack">
+		<!-- page -->
+		<div class="page" id="page-home">
+			<!-- Blueprint header -->
+			<header class="bp-header cf">
+				<span class="bp-header__present slideRight">WEB DEVELOPER | DESIGNER | CREATIVE</span>
+				<h1 class="bp-header__title bounce"><img src="images/Lames-logo-trans.png" width="200" height="222" alt="Lamar Harris Designs - Logo"></h1>
+				
+				
+			</header>
+		
+		</div>
+		<!-- /page -->
+        
+		<div class="page" id="page-lamar">
+        <div id="logo">Lamar Harris Designs</div>
+		<div class="tags">HTML5   CSS3   JavaScript <span>Designer</span>   jQuery Logos Visual  <span>Illustrator</span>  Wireframing   Mobile Responsive <span>Front–end</span> Web Development Interface Concept Project Manager Marketing <span>Creative</span> Graphics</div>
+          <header class="bp-header cf">
+			<h1 class="bp-header__title fadeIn">Lamar Harris</h1>
+			<p class="bp-header__desc">I design and develop creative online experiences. I'm not only a web developer, I play well with vectors.</p>
+			<p class="info">I've been part of First Data Corporation since  2006.  As an Application Developer,  I design, build, and launch new web  sites for community financial  institutions.  In addition, I works  closely with clients to project manage the setup of their hosting and site security along with domain name management. Normal everyday development consist of UI front-end framework, responsive web/mobile interfaces and online banking single-sign-on modules.</p>
+			<p class="info">I attended The Art Institute of Houston to persue a degree in Interactive Media and Web Design. Upon graduating I received the award for Best In Show Portfolio for 4th Quarter graduates in my program. </p>
+			<p class="info pulse"><a href="assets/Harris Resume.pdf" target="_blank"><span class="fa fa-file-pdf-o fa-2x"></span>Résumé</a></p>
+				
+		    </header>
+			
+		</div>
+        
+		<div class="page" id="page-work">
+        <div id="logo">Lamar Harris Designs</div>
+			<header class="bp-header cf">
+				<h1 class="bp-header__title">Work</h1>
+				<p class="bp-header__desc">UI/UX Design Wireframing Front–end Development with HTML5 CSS3 jQuery PHP</p>
+			  <ul id="triple">
+				  <li><a href="https://www.htbna.com/" target="_blank">HomeTown Bank, N.A.</a></li>
+				  <li><a href="http://www.mosemoodyheirsllc.com/" target="_blank">Mose Moody Heirs LLC.</a> <span class="fa fa-wordpress"></span></li>
+				  <li><a href="https://www.bsnb.com/" target="_blank">Ballston Spa National Bank</a></li>
+				  <li><a href="https://www.marionstatebanktx.com" target="_blank">Marion State Bank</a></li>
+				  <li><a href="http://abda.org/" target="_blank">Austin Bellydance Association</a> <span class="fa fa-wordpress"></span></li>
+				   <li><a href="https://nabankco.com/" target="_blank">North America Banking Company</a></li>
+                   <li><a href="http://flcct.org/" target="_blank">Financial Literacy Coalition - Central Texas</a> <span class="fa fa-wordpress"></span></li>
+				 
+			  </ul>
+				<p class="bp-header__desc">Just a few vectors</p>	
+				<div class="box two float-left"><img src="images/Diesel-Foods-Logo-Final.png" width="200" height="166" alt="Diesel Foods - Logo"></div>
+                <div class="box two float-left"><img src="images/JF-Final.png" alt="Lamar Middle School Jazz Factory - Logo" width="667" height="664"></div>
+                <div class="box two float-left"><img src="images/Final-MMR-Full.png" width="665" height="588"></div>
+                <div class="box two float-left last"><img src="images/cupcake-logo.png" width="200" height="258" alt="Cakes By Steph"></div>
+                <div class="cf"></div>
+			</header>
+			
+		</div>
+		
+		<div class="page" id="page-contact">
+        <div id="logo">Lamar Harris Designs</div>
+			<header class="bp-header cf">
+				<h1 class="bp-header__title">Contact</h1>
+				<p class="bp-header__desc">
+                <a href="tel:1-281-703-3450"><span class="fa fa-mobile"></span> (281) 703-3450</a>  |
+				<a href="mailto:lamarharrisdesigns@gmail.com"><span class="fa fa-envelope"></span></a> | 
+                <a href="https://www.linkedin.com/in/lamar-harris-4638b4135/"><span class="fa fa-linkedin"></span></a>
+                </p>
+				 <form id="#LamarForm" action="form.php" method="POST" enctype="multipart/form-data">
+       			<div class="form-row">
+                <input type="text" id="fullName" name="fullName" class="formInputs inputsToClear" placeholder="Your Full Name" required/>
+                </div>
+                <div class="form-row">
+            	<input type="email" id="email" name="email" class="formInputs inputsToClear" placeholder="Email *" required/>
+                </div>
+                <div class="form-row">
+              	<input type="text" id="subject" name="subject" class="formInputs inputsToClear" placeholder="Subject *" required/>
+                </div>
+                <div class="form-row">
+               	<textarea id="message" name="message" class="formInputs inputsToClear" rows="3" cols="20" maxlength="150" wrap="soft" placeholder="Message"></textarea>
+                </div>
+               
+                    <input type="submit" id="submit" name="submit" class="formInputs" value="Send Email"> <input type="reset" id="resetButton" name="reset" class="formInputs formInputsClear" value="Clear">       
+        </form>
+			</header>
+			<div class="poster"><img src="images/thomas.png" alt="Thomas Illustration" /></div>
+		</div>
+	</div>
+	<!-- /pages-stack -->
+	<button class="menu-button"><span>Menu</span></button>
+	<script src="js/classie.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
